b7acead3fc74c917e917debdc9fd89e296077ea4
artemkha <khaapt@gmail.com>
2017-10-01 21:59:32 +0300

    repeat problem

--- huffman.js
+++ huffman.js
@@ -1,5 +1,6 @@
-// const data = 'abasabsasascc'
 const data = 'beep boop beer!'
+const data1 = 'abasabsasascc'
+const data2 = 'ass'
 
 function count(string) {
   const str = string.split('')
@@ -16,17 +17,17 @@ function count(string) {
   return frequency
 }
 
-function sort(obj) {
+function toLeaves(obj) {
   const sortable = []
   for (let ltr in obj) {
     sortable.push([ltr, obj[ltr]])
   }
 
-  const tree = sortable.sort(sortByFrequency).map(leaf => {
+  const leaves = sortable.sort(sortByFrequency).map(leaf => {
     return Leaf(leaf[1], counter(), leaf[0])
   })
   
-  return tree
+  return leaves
 }
 
 function Node(value, link, left, right) {
@@ -34,7 +35,7 @@ function Node(value, link, left, right) {
 }
 
 function Leaf(value, link, char) {
-  return { type: 'Leaf', value: value, link: link, leaf: char}
+  return { type: 'Leaf', value: value, link: link, char: char}
 }
 
 const counter = (function () {
@@ -42,22 +43,7 @@ const counter = (function () {
   return function () { return count += 1; }
 })();
 
-function toHuffman(array) {
-  const n = array.length
-  const tree = []
-
-  while (array.length !== 1) {
-    const nodeFrequency = array[0][1] + array[1][1]
-    tree.push(array.splice(0, 2, [0, nodeFrequency]))
-    // console.log(array)
-  }
-  
-  tree.push(array.splice(0, 2, [0, array[0][1]]))
-  
-  return tree
-}
-
-function toHuffman2(tree) {
+function toTree(tree) {
   const n = tree.length - 1
   const temp = [...tree]
 
@@ -73,22 +59,6 @@ function toHuffman2(tree) {
   return tree
 }
 
-function toHuffman3(array) {
-  const n = array.length - 1
-  // const tree = [...array]
-  const tree = []
-
-  for (let k = n + 1; k <= (2 * n); k++) {
-    const one = extractMIN(array)[0]
-    const two = extractMIN(array)[0]
-    tree.push(one)
-    tree.push(two)
-    tree.push([0, one[1] + two[1]])
-    array.push([0, one[1] + two[1]])
-  }
-  return tree
-}
-
 function extractMIN(arr) {
   const min = arr.sort(sortByFrequency)
 
@@ -96,14 +66,43 @@ function extractMIN(arr) {
 }
 
 function sortByFrequency(a, b) {
-  console.log(a.value)
   return a.value - b.value
 }
 
-const sorted = sort(count(data))
-// console.log(sorted)
-// console.log(toHuffman2(sorted))
-console.log(toHuffman2(sorted))
+function toHuffman(tree) {
+  const root = tree[tree.length - 1]
+  
+  const code = {}
+
+  function toCode(node, acc) {
+    
+    if (node.type !== 'Leaf') {
+      const left = node.leaves[0]
+      const right = node.leaves[1]
+      toCode(tree[left], acc += 0)
+      toCode(tree[right], acc += 1)
+    } 
+
+    console.log(node)
+    if (node.char) {
+      code[node.char] = acc
+      console.log(acc)
+      acc = acc. slice(0, -1)
+      return 
+    }
+  }
+  
+  toCode(root, '')
+  return code
+}
+
+
+const leaves = toLeaves(count(data))
+const tree = toTree(leaves)
+
+console.log(toHuffman(tree))
+// console.log(toTree(leaves))
+// console.log(toTree(toLeaves(count(data))))
 
 // for (let k = n + 1; k <= 2 * n - 1; k++) {
 
